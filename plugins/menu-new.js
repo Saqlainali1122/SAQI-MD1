81c8de6050cbe85288c1e2dfe5e82a3d350f4d5f
Saqlainali1122 <saqlainali9200@gmail.com>
2025-11-16 00:00:38 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,8 +39,8 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
-                newsletterName: config.OWNER_NAME,
+                newsletterJid: '120363403667982036@g.us',
+                newsletterName: config.Saqi_md,
                 serverMessageId: 143
             }
         };
